a926b982fe8c616e782fa534b26fe6043cd403bf
jd-13 <jackd8989@gmail.com>
2016-09-21 22:34:02 +0100

    Update headers

--- CarveLookAndFeel.cpp
+++ CarveLookAndFeel.cpp
@@ -1,12 +1,26 @@
 /*
-  ==============================================================================
-
-    CarveLookAndFeel.cpp
-    Created: 17 Sep 2015 10:39:00pm
-    Author:  Jack Devlin
-
-  ==============================================================================
-*/
+ *	File:		CarveLookAndFeel.cpp
+ *
+ *	Version:	2.0.0
+ *
+ *	Created:	17/09/2015
+ *
+ *	This file is part of Carve.
+ *
+ *  Carve is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Carve is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Carve.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #include "CarveLookAndFeel.h"
 
