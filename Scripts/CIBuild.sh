4e83e1155161c3542748ad18bf817856ca3db8b0
jd-13 <jackd8989@gmail.com>
2020-10-09 22:37:59 +0100

    Create blank ttf files in CI

--- Scripts/CIBuild.sh
+++ Scripts/CIBuild.sh
@@ -9,6 +9,10 @@ WECORE_HOME="$(cd $SCRIPT_DIR/.. > /dev/null 2>&1 && pwd)"
 WECORE_SRC="$WECORE_HOME/WECore"
 git clone https://github.com/jd-13/WE-Core $SCRIPT_DIR/../WECore
 
+# We can't download the fonts so create blank files in their place
+touch $SCRIPT_DIR/../Source/Graphics/Montserrat-Bold.ttf
+touch $SCRIPT_DIR/../Source/Graphics/Montserrat-Regular.ttf
+
 # Set JUCE module paths
 sed -i 's#../JUCE/modules#/home/JUCE/modules#g' $SCRIPT_DIR/../Carve.jucer
 
